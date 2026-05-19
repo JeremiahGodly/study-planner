cc7efdd2dc6c36921f1c7f8d0ffc67e8ce974413
Jeremiah Godly Varghese <jrmhgodly@gmail.com>
2026-05-19 20:34:11 +0530

    Initial commit

---
+++ study_plannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  study_plannerApp.swift
+//  study planner
+//
+//  Created by Godly Varghese on 19/05/26.
+//
+
+import SwiftUI
+
+@main
+struct study_plannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
